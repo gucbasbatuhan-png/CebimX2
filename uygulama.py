a6e0f5a41f84cd5f0f6c1485174dde0578bd95d5
gucbasbatuhan-png <gucbasbatuhan@gmail.com>
2026-03-31 23:33:36 +0300

    Update uygulama.py

--- uygulama.py
+++ uygulama.py
@@ -218,7 +218,7 @@ if df_yastik.empty:
     clear_cache_and_rerun()
 
 kategoriler = ["Market", "Kira", "Fatura", "Eğlence", "Oyun & Yazılım", "Donanım (Al-Sat)", "Diğer", "Proje & Geliştirici", "Eğitim", "Kişisel Gelişim", "Dışarıdan Yeme", "Dışarıdan İçme", "Ulaşım", "Seyahat", "Giyim", 
-              "Kişisel Bakım", "Sağlık", "Eczane", "Berber", "Büşra Kuaför", "Elektrik", "Su", "Doğalgaz", "İnternet", "Aidat", "Depo Kira", "Büşra Telefon", "Batu Telefon"]
+              "Kişisel Bakım", "Ev Kredisi", "Sağlık", "Eczane", "Berber", "Büşra Kuaför", "Elektrik", "Su", "Doğalgaz", "İnternet", "Aidat", "Depo Kira", "Büşra Telefon", "Batu Telefon"]
 
 if df_butceler.empty:
     for i, kat in enumerate(kategoriler):
